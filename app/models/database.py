fdd828b1570f71c4648f1a685b68914ac0506d28
ZanZanar <fankds2@mail.ru>
2025-10-24 16:21:30 +0300

    chore(submodule): bump fastapi_notes_api to fafd1d9

--- app/models/database.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy import create_engine
-from fastapi_notes_api.app.models.model import Base
-
-engine = create_engine("sqlite+pysqlite:///fastapi_notes_api/app/db/test.db",echo=True)
-
-Base.metadata.create_all(engine)
